3950d5a111670834c830be9eb3a8bb7773e3070b
Harry <harrygross1@gmail.com>
2025-07-09 13:57:10 +0100

    Add FILTER as an unlockable reward.

--- RPGTemplate/Assets/ExcelDomain/QuestionRewardsDatabase.cs
+++ RPGTemplate/Assets/ExcelDomain/QuestionRewardsDatabase.cs
@@ -25,7 +25,7 @@ namespace Assets.ExcelDomain
             { QuestionRewardClassification.AdvancedText, new string[] { "SUBSTITUTE", "REGEXREPLACE", "REGEXTEST", "MID", "UPPER", "DOLLAR", "FORMULATEXT", "ISNONTEXT", "DETECTLANGUAGE", "TRANSLATE", "CONCAT", "TEXTBEFORE", "TEXTJOIN", "TEXTSPLIT" }  },
             { QuestionRewardClassification.BasicManipulation, new string[] { "IFS", "EXPAND", "TRANSPOSE", "TOROW", "AREAS", "GESTEP", "TYPE", "ISOMITTED", "ISFORMULA", "ISLOGICAL", "IMAGE", "HYPERLINK", "NOT"    }  },
             { QuestionRewardClassification.AdvancedManipulation, new string[] { "SIGN", "LET", "LAMBDA", "IF", "OFFSET", "IFNA", "BYCOL", "BYROW", "MAKEARRAY", "MAP", "REDUCE", "SCAN", "TAKE", "DROP", "TOCOL"  }  },
-            { QuestionRewardClassification.AdvancedDates, new string[] { "DATE", "DATEVALUE", "DAY", "DAYS", "HOUR", "MINUTE", "MONTH", "NOW", "SECOND", "TIME", "TIMEVALUE", "YEAR", "YEARFRAC"  }  },
+            { QuestionRewardClassification.AdvancedDates, new string[] { "DATE", "DATEVALUE", "DAY", "DAYS", "HOUR", "MINUTE", "MONTH", "NOW", "SECOND", "TIME", "TIMEVALUE", "YEAR", "YEARFRAC", "FILTER" }  },
             { QuestionRewardClassification.ExpertDates, new string[] { "DAYS360", "EDATE", "EOMONTH", "ISOWEEKNUM", "NETWORKDAYS", "NETWORKDAYS.INTL", "TODAY", "WEEKDAY", "WEEKNUM", "WORKDAY", "WORKDAY.INTL", }  },
 
         };
