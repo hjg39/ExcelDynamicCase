165b38fa51d9967f5e7c3a8e413174a4b0dd7665
Harry <harrygross1@gmail.com>
2025-07-20 14:43:57 +0100

    Remove expert snake question.

--- RPGTemplate/Assets/Creator Kit - RPG/Logic/BattleManager.cs
+++ RPGTemplate/Assets/Creator Kit - RPG/Logic/BattleManager.cs
@@ -63,7 +63,7 @@ namespace Assets.Creator_Kit___RPG.Logic
                 case QuestionRewardClassification.ExpertBases:
                 case QuestionRewardClassification.ExpertMaths:
                 case QuestionRewardClassification.ExpertDates:
-                    return new int[] { 13, 33, 40020, 15, 40, 40007, 17, 30398, 40012, 24, 30390, 40015, 40023, 40024, 40025, 51088, 52111, 60003, 70000, 10017, 80001, 80002 };
+                    return new int[] { 13, 33, 40020, 15, 40, 17, 30398, 40012, 24, 30390, 40015, 40023, 40024, 40025, 51088, 52111, 60003, 70000, 10017, 80001, 80002 };
                 case QuestionRewardClassification.DivineAggregates:
                     return new int[] { 16, 31, 43, 76, 10016, 10018, 20000, 30378, 30354, 50104, 90000, 90001, 90002, 90003, 90004 };
                 case QuestionRewardClassification.BasicCurated:
