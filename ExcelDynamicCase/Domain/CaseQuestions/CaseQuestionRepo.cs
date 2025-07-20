4ed96b2edf4997f5bdab053dcde3447e1d391b38
Harry <harrygross1@gmail.com>
2025-07-20 13:49:29 +0100

    Dim's suggested answer fix for Lana.

--- ExcelDynamicCase/Domain/CaseQuestions/CaseQuestionRepo.cs
+++ ExcelDynamicCase/Domain/CaseQuestions/CaseQuestionRepo.cs
@@ -2948,7 +2948,7 @@ namespace ExcelDynamicCase.Domain.CaseQuestions
                             }
                         }
                     },
-                    Answer = "0001000101100001100000011000000100000001",
+                    Answer = "000100010110000110000010100000100000001",
                     ExampleAnswer = "'10101010100101010101010101010101000101",
                     Minutes = 45f,
                 }
